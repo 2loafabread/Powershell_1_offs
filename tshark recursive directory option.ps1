00586c01ab1f2b2de9883e2ce2d50648947c4407
2loafabread <60334094+2loafabread@users.noreply.github.com>
2024-10-29 09:11:17 -0700

    tshark recursive directory option.ps1
    adds recursive directory functionality to tshark... in a roundabout way I suppose

---
+++ tshark recursive directory option.ps1
@@ -0,0 +1,16 @@
+﻿# Set the directory containing your .pcap files
+$pcapDirectory = "C:\Users\Account\Downloads\PCAP"
+
+# Path to tshark executable
+$tsharkPath = "C:\Program Files\Wireshark\tshark.exe"
+
+# Loop through each .pcap file in the directory
+Get-ChildItem -Path $pcapDirectory -Filter *.pcapng | ForEach-Object {
+    $pcapFile = $_.FullName
+    Write-Host "Processing file: $pcapFile"
+
+    # Run tshark to display the protocol hierarchy for the current .pcap file
+    #& "$tsharkPath" -r $pcapFile -q -z io,phs
+    & "$tsharkPath"  -r $pcapFile -Y 'smb2 && smb2.filename' -T fields -e smb2.filename | sort -u
+
+}
